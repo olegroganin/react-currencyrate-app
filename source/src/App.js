676914b67881112e02a6d1905e7ecf4ae3293e1c
Oleg Roganin <94141484+olegroganin@users.noreply.github.com>
2022-01-15 17:33:37 +0200

    Add source code

---
+++ source/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Currencyrate from './components/Currencyrate';
+
+
+function App() {
+   return (
+      <div>
+         <Currencyrate/>
+		</div>
+   )
+}
+
+
+export default App;
